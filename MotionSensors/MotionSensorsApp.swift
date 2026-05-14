84a84103e644fd5e55436fc9f4dbb3005cf0f458
ReggieGrant <grantreggie09@gmail.com>
2026-05-13 17:14:04 -0700

    Initial Commit

---
+++ MotionSensors/MotionSensorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionSensorsApp.swift
+//  MotionSensors
+//
+//  Created by Reginald Grant on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct MotionSensorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
